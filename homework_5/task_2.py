39e01d383ebced7cb93ab89478b515e840594fad
Alexei <datlife2015@yandex.ru>
2023-08-14 22:16:53 +0300

    homeworh_5

--- homework_5/task_2.py
+++ homework_5/task_2.py
@@ -14,18 +14,10 @@ def calculate_bonus(salary, percentage):
     return salary * float(percentage.strip('%')) / 100
 
 
-def generate_bonus_dictionary(names, salaries, bonuses):
-    bonus_dict = {}
-    for name, salary, bonus in zip(names, salaries, bonuses):
-        bonus_amount = calculate_bonus(salary, bonus)
-        bonus_dict[name] = bonus_amount
-    return bonus_dict
-
-
 name_list = ['Vlad', 'Den', 'Alex']
 salary_list = [1000, 2000, 3000]
 extra_list = ['10.25%', '15%', '20%']
 
-bonus_dictionary = generate_bonus_dictionary(
-    name_list, salary_list, extra_list)
+bonus_dictionary = {name: calculate_bonus(salary, extra)for name, salary,
+                    extra in zip(name_list, salary_list, extra_list)}
 print(bonus_dictionary)
